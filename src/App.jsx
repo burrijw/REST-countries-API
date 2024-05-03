e753b4322d8606d86ea413832075779519229cf2
Josh Burri <burrijw@gmail.com>
2024-05-03 14:52:15 -0600

    convert the FilterRegion to be controlled

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,75 @@
-import './App.css'
-import HeaderPanel from './HeaderPanel'
-import CountriesSearchField from './CountrySearchField'
-import FilterRegion from './FilterRegion'
-import CountriesCard from './CountriesCard'
-import { useSearchParams } from 'react-router-dom'
-import React, { useState } from 'react'
-
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import "./App.css";
+import CountriesCard from "./CountriesCard";
+import CountriesSearchField from "./CountrySearchField";
+import FilterRegion from "./FilterRegion";
+import HeaderPanel from "./HeaderPanel";
 
 function App() {
   const [searchParams, setSearchParams] = useSearchParams({ country: "" });
-  const [regionParam, setRegionParam] = useSearchParams({ region: "" });
+  const [regionParam, setRegionParam] = useSearchParams();
 
-
-  let searchCountry = searchParams.get('country');
-  let filterRegion = regionParam.get('region');
-
+  let searchCountry = searchParams.get("country");
+  let filterRegion = regionParam.get("region");
 
   return (
-
-    <div className='min-h-screen flex flex-col bg-light-theme-background font-Neunito-font'>
-      <header className=''>
+    <div className="min-h-screen flex flex-col bg-light-theme-background font-Neunito-font">
+      <header className="">
         <HeaderPanel />
       </header>
-      <main className='md:mx-10'>
-        <h1 className='sr-only'>Rest Countries API</h1>
-        <div className='md:flex items-center justify-between'>
-          <CountriesSearchField searchParams={searchParams} setSearchParams={setSearchParams} searchCountry={searchCountry} />
-          <FilterRegion setRegionParam={setRegionParam} regionParam={regionParam} filterRegion={filterRegion} />
+      <main className="md:mx-10">
+        <h1 className="sr-only">Rest Countries API</h1>
+        <div className="md:flex items-center justify-between">
+          <CountriesSearchField
+            searchParams={searchParams}
+            setSearchParams={setSearchParams}
+            searchCountry={searchCountry}
+          />
+          <FilterRegion
+            setRegionParam={setRegionParam}
+            regionParam={regionParam}
+            filterRegion={filterRegion}
+          />
         </div>
-        <CountriesCard searchCountry={searchCountry} filterRegion={filterRegion} />
+        <CountriesCard
+          searchCountry={searchCountry}
+          filterRegion={filterRegion}
+        />
 
-        <button className='fixed bottom-0 left-0 transform translate-x-1/2 bg-white shadow-md p-4 mb-6 rounded-full hover:scale-105 transition-transform'
-          aria-label='scroll to top button'
-          onClick={() => window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-          })}><svg className="w-6 h-6"
-            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
-            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
+        <button
+          className="fixed bottom-0 left-0 transform translate-x-1/2 bg-white shadow-md p-4 mb-6 rounded-full hover:scale-105 transition-transform"
+          aria-label="scroll to top button"
+          onClick={() =>
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            })
+          }
+        >
+          <svg
+            className="w-6 h-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m4.5 18.75 7.5-7.5 7.5 7.5"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m4.5 12.75 7.5-7.5 7.5 7.5"
+            />
           </svg>
         </button>
       </main>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/FilterRegion.jsx
+++ src/FilterRegion.jsx
@@ -1,95 +1,131 @@
-import * as Select from '@radix-ui/react-select'
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import * as Select from "@radix-ui/react-select";
+import React, { useEffect } from "react";
 
+function FilterRegion({ setRegionParam, regionParam, filterRegion }) {
+  const regionList = [
+    { id: 1, value: "Africa" },
+    { id: 2, value: "America" },
+    { id: 3, value: "Asia" },
+    { id: 4, value: "Europe" },
+    { id: 5, value: "Oceania" },
+  ];
 
-function FilterRegion({setRegionParam, regionParam, filterRegion}) {
-    const regionList = [
-        { id: 1, value: 'Africa' },
-        { id: 2, value: 'America' },
-        { id: 3, value: 'Asia' },
-        { id: 4, value: 'Europe' },
-        { id: 5, value: 'Oceania' },
-    ]
+  useEffect(() => {
+    console.log(filterRegion);
 
-    useEffect(() => {
-        console.log(filterRegion);
+    if (isNaN(filterRegion)) {
+      console.log("Null");
+    }
+  });
 
-        if(isNaN(filterRegion)){
-            console.log('Null');
+  return (
+    <form className="inline-flex items-center">
+      <Select.Root
+        name="region filter list"
+        value={filterRegion ?? ""}
+        onValueChange={(value) =>
+          setRegionParam(
+            (r) => {
+              r.set("region", value);
+              return r;
+            },
+            { replace: true }
+          )
         }
-    })
+      >
+        {filterRegion && (
+          <button
+            type="button"
+            aria-label="Remove filter"
+            onClick={() => {
+              regionParam.delete("region");
+              setRegionParam(regionParam);
+            }}
+            className="bg-white shadow-md p-2 mx-3 md:mx-0 rounded-full hover:bg-gray-200 transform hover:scale-105 transition-all"
+          >
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        )}
 
-    return (
-        <form className='inline-flex items-center'>
-            <Select.Root name='region filter list' defaultValue={filterRegion} onValueChange={(value) => setRegionParam(r => {
+        <Select.Trigger
+          className="bg-white inline-flex items-center shadow-md p-4 mx-6 rounded-md"
+          aria-label="Filter by Region"
+        >
+          <Select.Value
+            className="font-semibold"
+            placeholder="Filter by Region"
+          />
 
-                r.set('region', value);
-                return r
-            }, { replace: true })}>
+          <svg
+            className="w-4 ml-10"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </Select.Trigger>
 
-                {filterRegion && <button
-                    type='button'
-                    aria-label='Remove filter'
-                    onClick={() => {
+        <Select.Portal>
+          <Select.Content
+            position="popper"
+            sideOffset={5}
+            className="bg-white shadow-md rounded-lg p-2 overflow-hidden transform max-h-[--radix-select-content-available-height ] w-[--radix-select-trigger-width]"
+          >
+            <Select.Viewport className="hover:cursor-default text-lg">
+              {regionList.map((regions) => (
+                <Select.Item
+                  key={regions.id}
+                  className="flex items-center p-1 justify-between rounded-lg data-[highlighted]:bg-gray-100 data-[highlighted]:outline-none transition-colors"
+                  value={regions.value}
+                >
+                  <Select.ItemText>{regions.value}</Select.ItemText>
 
-                        regionParam.delete('region');
-                        setRegionParam(regionParam);
-                       
-                    }}
-                    className='bg-white shadow-md p-2 mx-3 md:mx-0 rounded-full hover:bg-gray-200 transform hover:scale-105 transition-all'>
-
-                    <svg className="w-6 h-6"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                  <Select.ItemIndicator>
+                    <svg
+                      className=""
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      ></path>
                     </svg>
-
-                </button>}
-
-                <Select.Trigger className='bg-white inline-flex items-center shadow-md p-4 mx-6 rounded-md'
-                    aria-label='Filter by Region' >
-                    <Select.Value className='font-semibold'
-                        placeholder="Filter by Region" />
-
-                    <svg className="w-4 ml-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                    </svg>
-                </Select.Trigger>
-
-                <Select.Portal>
-                    <Select.Content
-                        position='popper'
-                        sideOffset={5}
-                        className='bg-white shadow-md rounded-lg p-2 overflow-hidden transform max-h-[--radix-select-content-available-height ] w-[--radix-select-trigger-width]'>
-                        <Select.Viewport className='hover:cursor-default text-lg'>
-
-                            {regionList.map(regions =>
-                                <Select.Item key={regions.id}
-                                    className='flex items-center p-1 justify-between rounded-lg data-[highlighted]:bg-gray-100 data-[highlighted]:outline-none transition-colors'
-                                    value={regions.value}>
-
-                                    <Select.ItemText >
-                                        {regions.value}
-                                    </Select.ItemText>
-
-                                    <Select.ItemIndicator>
-                                        <svg className='' width="15" height="15"
-                                            viewBox="0 0 15 15"
-                                            fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
-                                                fill="currentColor" fillRule="evenodd" clipRule="evenodd">
-                                            </path>
-                                        </svg>
-                                    </Select.ItemIndicator>
-                                </Select.Item>)}
-
-                        </Select.Viewport>
-                        <Select.Arrow />
-                    </Select.Content>
-                </Select.Portal>
-            </Select.Root>
-        </form >
-    );
+                  </Select.ItemIndicator>
+                </Select.Item>
+              ))}
+            </Select.Viewport>
+            <Select.Arrow />
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+    </form>
+  );
 }
 
-export default FilterRegion
+export default FilterRegion;
